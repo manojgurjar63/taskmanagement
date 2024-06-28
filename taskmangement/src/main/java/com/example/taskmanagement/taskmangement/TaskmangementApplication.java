148bdec765bdec87171aca0453feead8f06a75b3
manojgurjar63 <manoj.bidhudi22@gmail.com>
2024-06-28 15:47:14 +0530

    first commit

---
+++ taskmangement/src/main/java/com/example/taskmanagement/taskmangement/TaskmangementApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagement.taskmangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmangementApplication.class, args);
+	}
+
+}
